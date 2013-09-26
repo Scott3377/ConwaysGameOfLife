7358687c995b4278cc829ccb769d773fe23c0b37
Scott Thomas <Scott3377@hotmail.com>
2013-09-26 14:07:32 -0500

    Initial commit

---
+++ ConwaysGameOfLife/AllTests.java
@@ -0,0 +1,12 @@
+package ConwaysGameOfLife;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ConwaysGameOfLifeIteratorTest.class,
+		ConwaysGameOfLifeTest.class })
+public class AllTests {
+
+}
